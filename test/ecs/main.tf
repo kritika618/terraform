af01b973edd01323447fd722a643922f6a6de68a
kritika sharma <kritikasharma2281@gmail.com>
2025-05-03 20:25:47 -0400

    updated file

--- test/ecs/main.tf
+++ test/ecs/main.tf
@@ -2,45 +2,27 @@ module "ecs" {
   source       = "../../.modules/aws/ecs"
   cluster_name = var.cluster_name
 
-  services = {
-    github-app = {
-      cpu    = 256
-      memory = 512
-
-      container_definitions = {
-        github-container = {
-          image     = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/github-action:1.1.1"
-          essential = true
-          port_mappings = [
-            {
-              containerPort = 80
-              protocol      = "tcp"
-            }
-          ]
-        }
-      }
-
-      subnet_ids = [
-        "subnet-0697385b41cf20408"
-      ]
-
-      security_group_rules = {
-        allow_http = {
-          type        = "ingress"
-          from_port   = 80
-          to_port     = 80
-          protocol    = "tcp"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-
-        allow_all_egress = {
-          type        = "egress"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      }
-    }
-  }
+}
+variable "services" {
+  description = "Map of ECS services with configuration"
+  type = map(object({
+    cpu    = number
+    memory = number
+    container_definitions = map(object({
+      image     = string
+      essential = bool
+      port_mappings = list(object({
+        containerPort = number
+        protocol      = string
+      }))
+    }))
+    subnet_ids = list(string)
+    security_group_rules = map(object({
+      type        = string
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+  }))
 }
--- test/ecs/variable.tf
+++ test/ecs/variable.tf
@@ -7,9 +7,22 @@ variable "cluster_name" {
 variable "services" {
   description = "Map of ECS services with configuration"
   type = map(object({
-    cpu            = number
-    memory         = number
-    container_port = number
-    image          = string
+    memory = number
+    container_definitions = map(object({
+      image     = string
+      essential = bool
+      port_mappings = list(object({
+        containerPort = number
+        protocol      = string
+      }))
+    }))
+    subnet_ids = list(string)
+    security_group_rules = map(object({
+      type        = string
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
   }))
 }
